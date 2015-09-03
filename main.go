23580d1f8dddbb30ead849ad09bd00e83ea24c8e
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2015-09-03 12:20:18 +0200

    added openshift env variable, added .godir

--- main.go
+++ main.go
@@ -7,14 +7,17 @@ import (
 )
 
 func main() {
-	port := "8080"
+	addr := ":8080"
 	if env := os.Getenv("PORT"); env != "" {
-		port = env
+		addr = ":" + env
+	}
+	if env := os.Getenv("OPENSHIFT_GO_PORT"); env != "" {
+		addr = os.Getenv("OPENSHIFT_GO_IP") + ":" + env
 	}
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
-	r.Run(":" + port)
+	r.Run(addr)
 }
